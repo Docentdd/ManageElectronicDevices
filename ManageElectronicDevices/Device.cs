29b3e0a270c30a69c1d61739657f2ad1f412f325
DocentNDV <nakonechndima@gmail.com>
2025-03-19 14:47:04 +0100

    try to create the test for the smartwatch

--- ManageElectronicDevices/Device.cs
+++ ManageElectronicDevices/Device.cs
@@ -1,10 +1,80 @@
 namespace ManageElectronicDevices;
 
-public interface Device
+public abstract class Device
 {
-     string Id { get; set; }
-     string Name { get; set; }
-     bool isTurnedOn { get; set; }
+     public string Id { get; set; }
+     public string Name { get; set; }
+     public bool IsTurnedOn { get; set; }
 
-     
+     public Device(string id, string name, bool isTurnedOn)
+     {
+         Id = id;
+         Name = name;
+         IsTurnedOn = isTurnedOn;
+     }
+
+     public Device()
+     {
+     }
+     public abstract bool TurnOff();
+     public abstract void TurnOn();
+    
+}
+
+public class SmartWatch : Device, IPowerNotification
+{
+    private int _percentage;
+    public SmartWatch(string id, string name, bool isTurnedOn, int percentage) : base(id, name, isTurnedOn)
+    {
+        BatteryPercentage = percentage;
+    }
+
+    public override bool TurnOff()
+    {
+        IsTurnedOn = false;
+        return IsTurnedOn;
+    }
+
+    public override void TurnOn()
+    {
+        if (BatteryPercentage <= 11)
+        {
+            Console.WriteLine(_percentage + "- your percentage (subtract 10%)");
+        }
+        IsTurnedOn = true;
+        BatteryPercentage-= 10;
+    }
+
+    public int BatteryPercentage
+    {
+        get => _percentage;
+        set
+        {
+            if (_percentage < 0 || _percentage > 100)
+            {
+                throw new ArgumentOutOfRangeException("Battery percentage must be between 0 and 100");
+            }
+
+            if (BatteryPercentage < 20)
+            {
+                NotifyPower(_percentage);
+            }
+        }
+    }
+
+    public void NotifyPower(int percentage)
+    {
+        throw new Exception("This device " + Name + " need to be plugged in");
+    }
+}
+public interface IPowerNotification
+{
+    void NotifyPower(int percentage);
+}
+
+public class EmptyBatteryException : Exception
+{
+    public EmptyBatteryException(string message) : base(message)
+    {
+    }
 }
--- ManageElectronicDevices/Program.cs
+++ ManageElectronicDevices/Program.cs
@@ -1,1 +1,30 @@
-﻿
+using System;
+using System.Collections.Generic;
+
+namespace ManageElectronicDevices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Device> devices = new List<Device>
+            {
+                new SmartWatch("SW001", "Smartwatch 1", false, 50),
+                new SmartWatch("SW002", "Smartwatch 2", true, 11)
+            };
+
+            foreach (var device in devices)
+            {
+                try
+                {
+                    device.TurnOn();
+                    Console.WriteLine($"{device.Name} is turned on.");
+                }
+                catch (EmptyBatteryException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
